a17d36f7779ee3179278753c74b83a15e909046a
Omkar <omkar18rajmane@gmail.com>
2023-09-18 10:26:08 +0530

    made cpu page fully

--- Frontend/src/pages/CPU.jsx
+++ Frontend/src/pages/CPU.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import SideBar from '../components/SideBar'
+import React, { useEffect, useRef, useState } from 'react';
+import SideBar from '../components/SideBar';
+import ProductCard from '../components/productCard';
+import { Stack } from '@mui/system';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 const CPU = () => {
   const d1 = {
     label: 'Manufacturer',
@@ -116,10 +120,60 @@ const CPU = () => {
     const stackStyle = {
       marginTop: '1vh',
     };
-  const cat_titles=['Yes','NO']
-  const cat_label='Integrated Graphics'
+    const theme = createTheme({
+      typography: {
+        fontFamily: 'poppins, montserrat, sans-serif',
+      },
+      backgroundColor:'#373538',
+    });
+    const maxProductCardsPerStack = 3;
+
+    // Calculate the number of product cards per stack based on screen width
+    const calculateProductCardsPerStack = () => {
+      const screenWidth = window.innerWidth;
+  
+      let productCardsPerStack = maxProductCardsPerStack;
+  
+      if (screenWidth < 1000) {
+        productCardsPerStack = 1; // Adjust this based on your desired breakpoint
+      } else if (screenWidth < 1400) {
+        productCardsPerStack = 2; // Adjust this based on your desired breakpoint
+      }
+  
+      return productCardsPerStack;
+    };
+    const totalProductCards = 9;
+    // Calculate the number of stacks based on the number of product cards
+    const calculateStacks = () => {
+      let productCardsPerStack = calculateProductCardsPerStack();
+       // Total number of product cards
+      let totalStacks = Math.ceil(totalProductCards / productCardsPerStack);
+      if((totalStacks*productCardsPerStack)<totalProductCards){
+        totalStacks+=1;
+      }
+      return totalStacks;
+    };
+  
+    // Initialize state to hold the number of product cards per stack and the number of stacks
+    const [productCardsPerStack, setProductCardsPerStack] = useState(calculateProductCardsPerStack());
+    const [totalStacks, setTotalStacks] = useState(calculateStacks());
+  
+    // Update the number of product cards per stack and the number of stacks when the screen is resized
+    const handleResize = () => {
+      setProductCardsPerStack(calculateProductCardsPerStack());
+      setTotalStacks(calculateStacks());
+    };
+    useEffect(() => {
+      // Listen for window resize events and update the layout
+      window.addEventListener('resize', handleResize);
+  
+      // Clean up the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
   return (
-    <>
+    <ThemeProvider theme={theme}>
     <style>
         {`
           body {
@@ -127,10 +181,28 @@ const CPU = () => {
           }
         `}
       </style>
-   
+      <div style={containerStyle}>
       <SideBar drop={drop_1} slider={main_slider} sliderNum={slider_Num} checkboxCategories={checkbox}></SideBar>
-   
-    </>
+      <div style={productCardStyle}>
+          {[...Array(totalStacks)].map((_, stackIndex) => {
+            // Determine how many product cards should be in this stack
+            const curr_stack=stackIndex+1;
+            let num=curr_stack*productCardsPerStack
+            const cardsInThisStack = stackIndex === totalStacks - 1 && num > totalProductCards ? 1 : productCardsPerStack;
+
+            // Render a stack with the appropriate number of product crds
+            return (
+              <Stack key={stackIndex} direction="row" spacing={'0.5vw'} style={stackStyle}>
+                {[...Array(cardsInThisStack)].map((_, cardIndex) => (
+                  // Render a product card with spacing
+                  <ProductCard key={cardIndex} style={{ marginTop: cardIndex > 0 ? '1vh' : '0' }} />
+                ))}
+              </Stack>
+            );
+          })}
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
