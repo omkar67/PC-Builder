3f0cc08b57424ff719d1c585d58d8a43090695d4
Rajat-Rathaur <rajatrathaur.matrix@gmail.com>
2023-10-08 21:01:33 +0530

    mysql

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,30 @@ import { addSampleUser } from './queriesDB.js';
 
 const app = express();
 const port = 3000;
+///////////////
 
+import mysql from 'mysql';
+const db = mysql.createConnection({
+    host: "db4free.net",
+    user: "rajatmit",
+    password: "rajat@123",
+    database: "myinventory"
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to the database');
+});
+
+
+
+
+
+//////////////////
 // Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://rajatrathaurmatrix:w1s1TXWFmtVPlZdk@pc-builder-cluster1.vte5idg.mongodb.net/?retryWrites=true&w=majority', {
+/* mongoose.connect('mongodb+srv://rajatrathaurmatrix:w1s1TXWFmtVPlZdk@pc-builder-cluster1.vte5idg.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -23,7 +44,7 @@ app.get('/', async (req, res) => {
         res.send('Hello World! Error saving user.');
     }
 });
-
+ */
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
